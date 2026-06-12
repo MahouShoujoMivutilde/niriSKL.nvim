992ece17617190215be8c41f00b3d500cfdc8e65
MahouShoujoMivutilde <bbeedsi4@anonaddy.me>
2026-06-12 19:34:47 +0300

    when niriSKL isn't running due env requirements, let NiriSKL _dump_state explain exactly why

--- lua/niriSKL/env_requirements.lua
+++ lua/niriSKL/env_requirements.lua
@@ -1,18 +1,30 @@
 M = {}
 
-local function a(_) end
+local function noop(_) end
 
-local function b(_)
-    local msg = "[niriSKL] not running, state:\n"
+local function explain(_)
+    local msg = "[niriSKL] not running, state:\n\n"
 
-    msg = msg .. ("$XDG_CURRENT_DESKTOP = %s, (should be niri)\n"):format(vim.env.XDG_CURRENT_DESKTOP)
-    msg = msg .. ("$SSH_TTY = %s, (shouldn't be defined, nil)\n"):format(vim.env.SSH_TTY)
-    msg = msg .. ("vim.system() is %s, (should be a function, not nil)\n"):format(vim.inspect(vim.system))
+    if vim.env.XDG_CURRENT_DESKTOP ~= "niri" then
+        msg = msg .. ("$XDG_CURRENT_DESKTOP = %s, but should be niri.\n"):format(vim.env.XDG_CURRENT_DESKTOP)
+    end
+
+    if vim.env.SSH_TTY ~= nil then
+        msg = msg .. ("$SSH_TTY = %s, but shouldn't be defined. Are we running from ssh session?\n"):format(vim.env.SSH_TTY)
+    end
+
+    if vim.system == nil then
+        msg = msg .. ("vim.system() is %s, but should be a function. Is neovim pre v0.10.0?\n"):format(vim.inspect(vim.system))
+    end
+
+    if M.are_we_good() then
+        return
+    end
 
     vim.notify(msg, vim.log.levels.WARN)
 end
 
-M.mock = { setup = a, launch = a, stop = a, _dump_state = b }
+M.mock = { setup = noop, launch = noop, stop = noop, _dump_state = explain }
 
 function M.are_we_good()
     if vim.env.XDG_CURRENT_DESKTOP ~= "niri" or vim.env.SSH_TTY ~= nil or vim.system == nil then
